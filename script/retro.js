7496261c753035724a9209df7170fd3f6b1b07ae
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-03-03 00:00:21 +0600

    added the letest news section

--- script/retro.js
+++ script/retro.js
@@ -1,1 +1,3 @@
 console.log('linked')
+
+{"id":101,"category":"Comedy","image":"https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg","isActive":true,"title":"10 Kids Unaware of Their Costume","author":{"name":"John Doe"},"description":"It is one thing to subject yourself to a costume mishap","comment_count":560,"view_count":1568,"posted_time":5}
