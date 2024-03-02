deb13448fcd5238bc99a71be5e849c3bc8e5d3be
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-03-03 02:13:36 +0600

    added the search bar related js

--- script/retro.js
+++ script/retro.js
@@ -1,54 +1,82 @@
-// {"id":101,"category":"Comedy",
+// {"id":101,
+//"category":"Comedy",
 // "image":"https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg",
 // "isActive":true,
 // "title":"10 Kids Unaware of Their Costume",
 // "author":{"name":"John Doe"},
-//     "description":"It is one thing to subject yourself to a costume mishap",
-//     "comment_count":560,
+// "description":"It is one thing to subject yourself to a costume mishap",
+// "comment_count":560,
 // "view_count":1568,
 // "posted_time":5}
 
 
-const showCard = async(category)=>{
+const showCard = async (search) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
     const data = await res.json()
     const result = data.posts
-    console.log(category)
-    displayCard(result)
+    data.posts.forEach(element => {
+        console.log(element)
+        if (element.category === search) {
+            displayCard(result)
+        }
+        else {
+            console.log('Kindly provide valid search keyword')
+
+        }
+    });
+
+    console.log(search)
+
 }
 
-const searchField = document.getElementById('search-field')
-const searchText = searchField.value
-showCard(searchText)
 
-const displayCard = (result)=>{
+const handleSearchButton = () => {
+    // toggleLoadingSpinner(true)
+    const alartText=document.getElementById('alart-text')
+    alartText.classList.add('hidden')
+    console.log('hoice button tipa')
+    const searchField = document.getElementById('search-field')
+    let searchText = searchField.value        
+    if(searchText === 'Comedy' || searchText === 'Music' || searchText === 'Coding'){
+        showCard(searchText)
+        searchField.value=""
+    }
+    else{
+        alartText.classList.remove('hidden')
+        searchField.value=""
+    }
+
+    
+}
+
+const displayCard = (result) => {
     console.log(result)
     const commentContainer = document.getElementById('comment-container')
-    result.forEach(element => {
+    commentContainer.innerText= ""
+    result.forEach((element) => {
         const div = document.createElement("div")
-        
-        div.innerHTML=`
-        <div class="avatar online h-[72px] w-[72px]">
-        <div class="w-24 rounded-xl">
-        <img src="https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg" />
-        </div>
+
+        div.innerHTML = `
+                    <div
+                        class="h-[350px] lg:h-[250px] w-full flex gap-2 lg:gap-6 flex-1 p-4 lg:p-10 border-2  border-[#797DFC] rounded-3xl flex-col lg:flex-row">
+                        <div class="avatar online h-[72px] w-[72px]">
+                            <div class="w-24 rounded-xl">
+                                <img src="https://i.ibb.co/0QRxkd5/pexels-jan-kop-iva-3525908.jpg" />
+                            </div>
                         </div>
                         <!-- card details section  -->
                         <div>
-                        <div class="border-b-2 border-dashed border-[#12132D40] mb-1 lg:mb-3 pb-1 lg:pb-4">
+                            <div class="border-b-2 border-dashed border-[#12132D40] mb-1 lg:mb-3 pb-1 lg:pb-4">
                                 <div
                                     class="flex flex-col lg:flex-row gap-1 lg:gap-5 font-inter font-medium text-sm lg:text-base">
-                                    <p># Music</p>
-                                    <p>Author : Awlad Hossain</p>
+                                    <p># ${element.category}</p>
+                                    <p>Author : ${element.author.name}</p>
                                 </div>
-                                <h3 class="text-base lg:text-[20px] font-bold text-[#12132D]">10 Kids Unaware of Their
-                                    Halloween Costume</h3>
-                                <p class="font-inter text-[#12132D99] text-sm lg:text-base">It’s one thing to subject
-                                    yourself to ha Halloween costume mishap because, hey that’s
-                                    your prerogative</p>
+                                <h3 class="text-base lg:text-[20px] font-bold text-[#12132D]">${element.title}</h3>
+                                <p class="font-inter text-[#12132D99] text-sm lg:text-base">${element.description}</p>
 
                             </div>
-                            <div class="flex justify-between flex-col lg:flex-row gap-2 lg:gap-0">
+                            <div class="flex justify-between mb-7 flex-col lg:flex-row gap-2 lg:gap-0 w-[580px]">
                                 <div class="flex gap-4">
                                     <!-- comment section  -->
                                     <div class="flex gap-3 items-center">
@@ -57,9 +85,9 @@ const displayCard = (result)=>{
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                                         </svg>
-                                        <span class="text-[#12132D99]">542</span>
-                                        <!-- view section  -->
+                                        <span class="text-[#12132D99]">${element.comment_count}</span>
                                     </div>
+                                    <!-- view section  -->
                                     <div class="flex gap-3 items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-[#12132D99]">
@@ -69,7 +97,7 @@ const displayCard = (result)=>{
                                                 d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                         </svg>
 
-                                        <span class="text-[#12132D99]">542</span>
+                                        <span class="text-[#12132D99]">${element.view_count}</span>
                                         <!-- time section  -->
                                     </div>
                                     <div class="flex gap-3 items-center">
@@ -78,7 +106,7 @@ const displayCard = (result)=>{
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                         </svg>
-                                        <span class="text-[#12132D99]">5 min</span>
+                                        <span class="text-[#12132D99]">${element.posted_time} Min</span>
                                     </div>
                                 </div>
                                 <div> <button class="btn rounded-full bg-[#10B981] w-13 h-13 p-3"><svg
@@ -91,9 +119,11 @@ const displayCard = (result)=>{
                                 </div>
                             </div>
                         </div>
-        `
+                    </div>
+    `
+
         console.log(element)
-        
+        commentContainer.appendChild(div)
     });
 }
 
